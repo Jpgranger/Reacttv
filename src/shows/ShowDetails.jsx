1eae87fc6471094b1209de5d0bcb8a23cc107a29
Jpgranger <joey1027@live.com>
2025-07-07 16:38:32 -0500

    donzo

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
 import "./shows.css";
 
-/** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+
+  if (!show) {
+    return <p className="show-details">Please select a show to watch.</p>;
+  }
+
+  return (
+    <div className="show-details">
+      <EpisodeList
+        episodes={show.episodes}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
+  );
 }
